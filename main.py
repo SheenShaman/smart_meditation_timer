d9459193f761cd50cc1c41734d8977d40b48033e
Danil Sheen <dan.80956@gmail.com>
2026-02-26 16:54:15 +0300

    add_temp_screens

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from smart-meditation-timer!")
+from kivy.app import App
+from kivy.uix.button import Button
 
+class MyApp(App):
+    def build(self):
+        return Button(text='Привет, это Kivy!', font_size=30)
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    MyApp().run()
