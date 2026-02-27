ecca59ffd01da7d2383b3bcef34ae1943a1476d1
Danil Sheen <dan.80956@gmail.com>
2026-02-27 10:29:55 +0300

    first_vers_app

--- main.py
+++ main.py
@@ -1,9 +1,41 @@
 from kivy.app import App
-from kivy.uix.button import Button
+from kivy.config import Config
+from kivy.factory import Factory
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager
 
-class MyApp(App):
+from screens import MeditationScreen, SettingsScreen, StatsScreen
+from core.theme import Theme
+
+Config.set("kivy", "log_level", "debug")
+
+Factory.register("MeditationScreen", cls=MeditationScreen)
+Factory.register("StatsScreen", cls=StatsScreen)
+Factory.register("SettingsScreen", cls=SettingsScreen)
+
+
+class RootManager(ScreenManager):
+    pass
+
+
+class MeditationApp(App):
+    """
+    Meditation App
+    """
+    theme = Theme()
+
+    def build_config(self, config):
+        config.setdefaults('graphics', {
+            'width': '400',
+            'height': '600',
+            'maxfps': '60',
+            'resizable': '0'
+        })
+
     def build(self):
-        return Button(text='Привет, это Kivy!', font_size=30)
+        Builder.load_file("meditation.kv")
+        return RootManager()
 
-if __name__ == '__main__':
-    MyApp().run()
+
+if __name__ == "__main__":
+    MeditationApp().run()
