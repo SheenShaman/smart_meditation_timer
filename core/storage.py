da704a2e8770aec57f355ff2c08a37b5a8d06e40
Danila Koryakin <91043795+SheenShaman@users.noreply.github.com>
2026-03-01 19:09:01 +0300

    Fix/danila/fix main screen (#2)
    * fix_screen_&_add_switch
    
    * add_switch_screen_&_fix_themes
    
    * linters

--- core/storage.py
+++ core/storage.py
@@ -1,7 +1,6 @@
 import json
 from pathlib import Path
 
-
 DATA_DIR = Path("data")
 DATA_FILE = DATA_DIR / "app_data.json"
 DEFAULT_DATA = {
@@ -25,10 +24,12 @@ def is_data_file():
         with open(DATA_FILE, "w", encoding="utf-8") as f:
             json.dump(DEFAULT_DATA, f, indent=4)
 
+
 def load_data_file():
     is_data_file()
     with open(DATA_FILE, "r", encoding="utf-8") as f:
         return json.load(f)
+
 
 def save_data_file(data: dict):
     with open(DATA_FILE, "w", encoding="utf-8") as f:
