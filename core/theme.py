7338fff2952f2cab679634bfa0dbde5deee4ef63
Danil Sheen <dan.80956@gmail.com>
2026-03-01 14:50:57 +0300

    add_switch_screen_&_fix_themes

--- core/theme.py
+++ core/theme.py
@@ -1,27 +1,11 @@
+from kivy.event import EventDispatcher
+from kivy.properties import ColorProperty
 from kivy.utils import get_color_from_hex
 
-class Theme:
-    PRIMARY = "#667eea"
-    ACCENT = "#764ba2"
-    BACKGROUND = "#1a1a2e"
-    LIGHT = "#e94560"
 
-    @staticmethod
-    def hex_to_rgb(hex_color: str):
-        return get_color_from_hex(hex_color)
-
-    @classmethod
-    def primary_rgba(cls):
-        return cls.hex_to_rgb(cls.PRIMARY)
-
-    @classmethod
-    def accent_rgba(cls):
-        return cls.hex_to_rgb(cls.ACCENT)
-
-    @classmethod
-    def bg_rgba(cls):
-        return cls.hex_to_rgb(cls.BACKGROUND)
-
-    @classmethod
-    def light_rgba(cls):
-        return cls.hex_to_rgb(cls.LIGHT)
+class Theme(EventDispatcher):
+    background = ColorProperty(get_color_from_hex("#1a1a2e"))
+    primary = ColorProperty(get_color_from_hex("#667eea"))
+    accent = ColorProperty(get_color_from_hex("#764ba2"))
+    light = ColorProperty(get_color_from_hex("#e94560"))
+    text = ColorProperty((1, 1, 1, 1))
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from kivy.app import App
 from kivy.config import Config
 from kivy.factory import Factory
-from kivy.lang import Builder
+from kivy.properties import StringProperty
 from kivy.uix.screenmanager import ScreenManager
 
-from screens import MeditationScreen, SettingsScreen, StatsScreen
 from core.theme import Theme
+from screens import MeditationScreen, SettingsScreen, StatsScreen
 
 Config.set("kivy", "log_level", "debug")
 
@@ -15,25 +15,32 @@ Factory.register("SettingsScreen", cls=SettingsScreen)
 
 
 class RootManager(ScreenManager):
-    pass
+    """
+    Менеджер экранов, отвечает за навигацию
+    """
+
+    current_screen_name = StringProperty("meditation")
+
+    def switch(self, screen_name: str):
+        if screen_name in self.screen_names:
+            self.current = screen_name
+            self.current_screen_name = screen_name
 
 
 class MeditationApp(App):
     """
     Meditation App
     """
+
     theme = Theme()
 
     def build_config(self, config):
-        config.setdefaults('graphics', {
-            'width': '400',
-            'height': '600',
-            'maxfps': '60',
-            'resizable': '0'
-        })
+        config.setdefaults(
+            "graphics",
+            {"width": "400", "height": "600", "maxfps": "60", "resizable": "0"},
+        )
 
     def build(self):
-        Builder.load_file("meditation.kv")
         return RootManager()
 
 
